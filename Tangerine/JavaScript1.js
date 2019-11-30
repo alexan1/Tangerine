1ed07cc755f916e80e1d288d65dc947a1cf49e1f
Alexandre Konioukhov <alexan1@gmail.com>
2019-11-29 21:17:02 -0500

    task4

---
+++ Tangerine/JavaScript1.js
@@ -0,0 +1,16 @@
+﻿var MyClass = function (x, y) {
+    "use strict";
+
+    this.first = x;
+    this.second = y;
+};
+
+MyClass.prototype.sayHi = function () {
+    $("#result").html(this.first + " " + this.second);
+    setTimeout(function () {
+        MyClass.prototype.sayHi.bind(this)
+    }, 1);
+};
+
+var c = new MyClass("Hello", "World");
+c.sayHi();
